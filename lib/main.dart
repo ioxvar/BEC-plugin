b197899ebb5a9d6a2381e2bf95a0a8945b109d8d
Rishabh <44425012+ioxvar@users.noreply.github.com>
2021-04-13 12:33:22 +0530

    Firebase test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './authentication_service.dart';
+import './home_page.dart';
+import './sign_in_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
@@ -11,90 +15,36 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Firebase Demo',
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.teal,
+          primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Firebase'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => FirebaseFirestore.instance
-                .collection('testing')
-                .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
-            child: Icon(Icons.add),
-          ),
-          body: StreamBuilder( 
-            stream: FirebaseFirestore.instance.collection('testing').snapshots(),
-            builder:(
-              BuildContext context,
-              AsyncSnapshot<QuerySnapshot> snapshot,
-            ),{
-              if(!snapshothasData) return const SizedBox.shrink();
-              return ListView.builder(
-                itemCount: snapshot.data.docs.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final docData = snapshot.data.docs[index].data();
-                  final DateTime = (docData['timestamp'] as Timestamp).toDate();
-                  return ListTile(
-                    title: Text(DateTime.toString()),
-                  )
-                }
-
-              )
-            }
-          ),
-        ));
-  }
-}
-/*
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+        home: AuthenticationWrapper(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-*/
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return HomePage();
+    }
+    return SignInPage();
+  }
+}
