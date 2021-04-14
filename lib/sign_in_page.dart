16c9310047e843d6554873f1e1658181a60cea09
Rishabh <44425012+ioxvar@users.noreply.github.com>
2021-04-14 22:16:29 +0530

    login UI

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -8,7 +8,7 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var logojpg = new AssetImage('assets/logo.jpg');
+    var logojpg = new AssetImage("assets/logo.jpg");
     var img = new Image(
       image: logojpg,
       width: 420,
@@ -17,28 +17,80 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          TextField(
-            controller: emailController,
-            decoration: InputDecoration(
-                labelText: 'EMAIL',
-                labelStyle: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.teal[600]))),
+          Container(
+            padding: EdgeInsets.fromLTRB(4.0, 102.0, 0.0, 0.0),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(2.0, 0.0, 0.0, 0.0),
+                  child: img,
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(65.0, 200.0, 0.0, 0.0),
+                  child: Text('BEC\tFaculty',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.teal[600].withOpacity(1),
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold)),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(132.0, 250.0, 0.0, 0.0),
+                  child: Text('Helper',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.teal[600].withOpacity(1),
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ],
+            ),
           ),
-          TextField(
-            controller: passwordController,
-            decoration: InputDecoration(
-                labelText: 'PASSWORD',
-                labelStyle: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green))),
-          ),
+          Container(
+              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        labelText: 'EMAIL',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.teal[600]))),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        labelText: 'PASSWORD',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 5.0),
+                  Container(
+                    alignment: Alignment(1.0, 0.0),
+                    padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                    child: InkWell(
+                      child: Text(
+                        'New User?',
+                        style: TextStyle(
+                            color: Colors.teal[600],
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat',
+                            decoration: TextDecoration.underline),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
           ElevatedButton(
             onPressed: () {
               context.read<AuthenticationService>().signIn(
@@ -46,7 +98,13 @@ class SignInPage extends StatelessWidget {
                     password: passwordController.text.trim(),
                   );
             },
-            child: Text("Sign in"),
+            child: Text(
+              'Sign in',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Montserrat'),
+            ),
           )
         ],
       ),
