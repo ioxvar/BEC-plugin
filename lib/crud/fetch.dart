0b1e31a0a8bd7b124cf6f5ab9cb47c4a9c344e2e
Rishabh <44425012+ioxvar@users.noreply.github.com>
2021-05-14 22:39:33 +0530

    fetch

--- lib/crud/fetch.dart
+++ lib/crud/fetch.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import './add.dart';
+final CollectionReference users = FirebaseFirestore.instance.collection('test');
+Stream collectionStream =
+    FirebaseFirestore.instance.collection('test').snapshots();
+//Stream documentStream = FirebaseFirestore.instance.collection('users').doc('ABC123').snapshots();
 
 class FetchData extends StatefulWidget {
   @override
@@ -9,21 +12,6 @@ class FetchData extends StatefulWidget {
 }
 
 class _FetchDataState extends State<FetchData> {
-  /*
-  Map data;
-
-  fetch() {
-    CollectionReference collectionReference =
-        FirebaseFirestore.instance.collection('test');
-    collectionReference.snapshots().listen((snapshot) {
-      List documents;
-      setState(() {
-        data = snapshot.documents[0].test;
-      });
-    });
-  }
-  */
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +21,41 @@ class _FetchDataState extends State<FetchData> {
         elevation: 1.0,
       ),
       body: Center(
-        child: Text('FETCH FROM HERE'),
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 80.0),
+              GestureDetector(
+                onTap: () async {
+                  users.get().then((querySnapshot) {
+                    querySnapshot.docs.forEach((result) {
+                      print(result.data());
+                    });
+                  });
+                },
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return Container(
+                        height: 40.0,
+                        width: 150.0,
+                        child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Colors.teal[600],
+                            color: Colors.teal[600],
+                            elevation: 7.0,
+                            child: Center(
+                                child: Text('FETCH DATA',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Montserrat')))));
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
