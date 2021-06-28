26ca7effea9e5b80d54ee2778667955dd4c30bfc
Rishabh <44425012+ioxvar@users.noreply.github.com>
2021-06-28 22:21:42 +0530

    Revert "profile page ui"
    This reverts commit eff1407b78583b7afd8a354f78fbb212105330e4.

--- lib/crud/profile.dart
+++ lib/crud/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class ProfilePage extends StatefulWidget {
   @override
   ProfilePageState createState() => ProfilePageState();
@@ -7,28 +8,28 @@ class ProfilePage extends StatefulWidget {
 
 class ProfilePageState extends State<ProfilePage> {
   final TextEditingController nameController = TextEditingController();
-  final TextEditingController deptController = TextEditingController();
-  final TextEditingController mailController = TextEditingController();
-
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: SingleChildScrollView(
         child: ConstrainedBox(
-          constraints: BoxConstraints(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
+            constraints: BoxConstraints(),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                    Widget>[
               Container(
                 child: Stack(
                   children: <Widget>[
                     Container(
                       padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
                       child: Text(
-                        'Edit Profile',
+                        'Signup',
                         style: TextStyle(
                             color: Colors.teal[600].withOpacity(1),
-                            fontSize: 60.0,
+                            fontSize: 80.0,
                             fontWeight: FontWeight.bold),
                       ),
                     ),
@@ -39,105 +40,104 @@ class ProfilePageState extends State<ProfilePage> {
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      controller: nameController,
-                      decoration: InputDecoration(
-                          hintText: 'Enter your name',
-                          hintStyle: TextStyle(color: Colors.grey[400]),
-                          labelText: 'Name',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.teal[600]))),
-                    ),
-                    SizedBox(height: 10.0),
-                    TextField(
-                      controller: deptController,
-                      decoration: InputDecoration(
-                          hintText: 'Enter your Department',
-                          hintStyle: TextStyle(color: Colors.grey[400]),
-                          labelText: 'Department',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.teal[600]))),
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 10.0),
-                    TextField(
-                      controller: mailController,
-                      decoration: InputDecoration(
-                          hintText: 'Enter your email',
-                          hintStyle: TextStyle(color: Colors.grey[400]),
-                          labelText: 'Email',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green))),
-                    ),
-                    SizedBox(height: 50.0),
-                    Container(
+                  padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                            hintText: 'something@email.com',
+                            hintStyle: TextStyle(color: Colors.grey[400]),
+                            labelText: 'EMAIL',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.teal[600]))),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                            hintText: '6+ characters',
+                            hintStyle: TextStyle(color: Colors.grey[400]),
+                            labelText: 'PASSWORD ',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.teal[600]))),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 10.0),
+                      TextField(
+                        controller: nameController,
+                        decoration: InputDecoration(
+                            hintText: 'Enter your name',
+                            hintStyle: TextStyle(color: Colors.grey[400]),
+                            labelText: 'Name',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green))),
+                      ),
+                      SizedBox(height: 50.0),
+                      Container(
+                          height: 40.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Colors.teal.shade600,
+                            color: Colors.teal[600],
+                            elevation: 7.0,
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Center(
+                                child: Text(
+                                  'SAVE',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat'),
+                                ),
+                              ),
+                            ),
+                          )),
+                      SizedBox(height: 20.0),
+                      Container(
                         height: 40.0,
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.0),
-                          shadowColor: Colors.teal.shade600,
-                          color: Colors.teal[600],
-                          elevation: 7.0,
-                          child: GestureDetector(
+                        color: Colors.transparent,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.black,
+                                  style: BorderStyle.solid,
+                                  width: 1.0),
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(20.0)),
+                          child: InkWell(
                             onTap: () {
                               Navigator.of(context).pop();
                             },
                             child: Center(
-                              child: Text(
-                                'SAVE',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat'),
-                              ),
+                              child: Text('Go Back',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat')),
                             ),
-                          ),
-                        ),),
-                    SizedBox(height: 20.0),
-                    Container(
-                      height: 40.0,
-                      color: Colors.transparent,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.black,
-                                style: BorderStyle.solid,
-                                width: 1.0),
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20.0)),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Center(
-                            child: Text('Go Back',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat')),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+                    ],
+                  )),
+            ])),
       ),
     );
   }
